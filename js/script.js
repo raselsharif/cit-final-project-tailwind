a234bf96fc307e2e56a9c23de74fce5b5bb719d8
Rasel Sharif <raselsharif9@gmail.com>
2023-09-17 12:50:56 +0600

    added slick slider

--- js/script.js
+++ js/script.js
@@ -1,3 +1,21 @@
 $(document).ready(function () {
-  $("#banner").slick();
+  // banner 
+  $(".banner").slick({
+    dots: false,
+    speed: 500,
+    prevArrow: ".prev",
+    nextArrow: ".next",
+  });
+  // testimonial
+
+  $(".testimonial").slick({
+    arrows: false,
+    // autoplay: true,
+    slidesToShow: 2,
+    slidesToScroll: 1,
+    // autoplaySpeed: 2000,
+    dots: false,
+    speed: 500,
+    dots: true,
+  });
 });
